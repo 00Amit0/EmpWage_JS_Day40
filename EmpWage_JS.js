a976ce311ed9a8a2d1db141b65cb7163d38220c2
“00Amit0” <amit836961@gmail.com>
2022-07-08 01:59:31 +0530

    While Loop in JS

--- EmpWage_JS.js
+++ EmpWage_JS.js
@@ -77,3 +77,19 @@
     let empWage=totalEmpHrs*WAGE_PER_HOUR;
     console.log("UC4 - Total Hrs:" +totalEmpHrs+"Emp Wage:"+empWage);
 }
+
+//UC5 - While loop
+{
+    const NUM_OF_WORKING_DAYS=20;
+    const MAX_HRS_IN_MONTH=160;
+    const WAGE_PER_HOUR=20;
+    let totalEmpHrs=0;
+    let totalWorkingDays=0;
+    while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+        totalWorkingDays++;
+        let empCheck=Math.floor(Math.random()*10)%3;
+        totalEmpHrs+=getWorkingHours(empCheck);
+    }
+    let empWage=totalEmpHrs*WAGE_PER_HOUR;
+    console.log("UC5 - Total Days:" +totalWorkingDays+ " Total Hrs:" +totalEmpHrs+" Emp Wage:"+empWage);
+}
