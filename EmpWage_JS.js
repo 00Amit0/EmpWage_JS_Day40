15758c3d942dbd2160b2de7433c489a6bd8aa6f2
“00Amit0” <amit836961@gmail.com>
2022-07-08 01:46:27 +0530

    Check employee is present or absent

--- EmpWage_JS.js
+++ EmpWage_JS.js
@@ -1,1 +1,12 @@
-//WELCOME TO JAVASCRIPT .. New Learning Same Concept 
+//WELCOME TO JAVASCRIPT .. New Learning Same Concept
+
+//UC1 -  Employee Present or Absent
+
+const IS_ABSENT=0
+    let empCheck=Math.floor(Math.random()*10)%2;
+    if(empCheck==IS_ABSENT)
+    {
+        console.log("UC1 - Employee is ABSENT. Exiting the Program");
+    }else{
+        console.log("UC1 - Employee is PRESENT");
+    }
