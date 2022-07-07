0e1a34d7d1c27dbd7438c5fa2f3342a652f93f9f
“00Amit0” <amit836961@gmail.com>
2022-07-08 01:57:57 +0530

    For Loop in JS

--- EmpWage_JS.js
+++ EmpWage_JS.js
@@ -64,3 +64,16 @@
     let empWage=empHrs*WAGE_PER_HOUR;
     console.log("Emp Hrs:"+empHrs+"Emp Wage:"+empWage);
 }
+
+//UC4 - For loop
+{
+    const NUM_OF_WORKING_DAYS=20;
+    const WAGE_PER_HOUR=20;
+    let totalEmpHrs=0;
+    for(let day=0;day<NUM_OF_WORKING_DAYS;day++){
+        let empCheck=Math.floor(Math.random()*10)%3;
+        totalEmpHrs+=getWorkingHours(empCheck);
+    }
+    let empWage=totalEmpHrs*WAGE_PER_HOUR;
+    console.log("UC4 - Total Hrs:" +totalEmpHrs+"Emp Wage:"+empWage);
+}
